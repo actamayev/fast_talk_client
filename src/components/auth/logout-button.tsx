149dd5b3c253abe895a1ddfe45349163672f08b2
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-23 15:14:59 -0400

    Logout classes

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -1,4 +1,5 @@
 import { observer } from "mobx-react"
+import { IoExitOutline } from "react-icons/io5"
 import Button from "../button"
 import useLogout from "../../hooks/auth/logout"
 
@@ -10,6 +11,8 @@ function LogoutButton() {
 			title="Logout"
 			colorClass="bg-red-500"
 			hoverClass="hover:bg-red-600"
+			className="text-white text-center text-sm rounded-full flex items-center justify-center h-8"
+			titleIcon={<IoExitOutline className="w-4 h-4" />}
 			onClick={logout}
 		/>
 	)
