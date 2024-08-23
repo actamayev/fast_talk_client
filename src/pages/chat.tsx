c74233c4caf725ec7b3620e268b8a7bef3b9dacf
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-23 12:30:21 -0400

    Update chat.tsx

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,13 +1,19 @@
 import _ from "lodash"
 import { observer } from "mobx-react"
 import { useParams } from "react-router-dom"
+import { useChatsContext } from "../contexts/chat-context"
 import BasicHelmet from "../components/helmet/basic-helmet"
+import { removeLeadingAt } from "../utils/leading-at-operations"
 import useRetrieveMessagesFromChatUseEffect from "../hooks/chat/retrieve-messages-from-chat-use-effect"
 
 function Chat() {
 	const { friendUsername } = useParams<{ friendUsername: AtPrefixedString }>()
+	const chatsClass = useChatsContext()
 	useRetrieveMessagesFromChatUseEffect(friendUsername)
+
 	if (_.isUndefined(friendUsername)) return null
+	const chat  = chatsClass.contextForChatByFriendUsername(removeLeadingAt(friendUsername))
+	if (_.isUndefined(chat)) return null
 
 	return (
 		<>
@@ -16,6 +22,11 @@ function Chat() {
 				description={`Chat with ${friendUsername}`}
 				url={`http://localhost:3000/c/${friendUsername}`}
 			/>
+			{chat.messagesArray.map(singleMessage => (
+				<div key={singleMessage.messageId}>
+					{singleMessage.text}
+				</div>
+			))}
 		</>
 	)
 }
