cf21b3f25d7e47cc24b9281099f29da22958164e
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-26 17:21:40 -0400

    Update set-default-site-theme.ts

--- src/hooks/set-default-site-theme.ts
+++ src/hooks/set-default-site-theme.ts
@@ -3,7 +3,6 @@ import { useAuthContext } from "../contexts/auth-context"
 import useDefaultSiteTheme from "./memos/default-site-theme"
 
 export default function useSetDefaultSiteTheme(): () => void {
-	// const fortunaApiClient = useApiClientContext()
 	const authClass = useAuthContext()
 	const defaultSiteTheme = useDefaultSiteTheme()
 
@@ -11,12 +10,6 @@ export default function useSetDefaultSiteTheme(): () => void {
 		try {
 			const newSiteTheme = defaultSiteTheme === "light" ? "dark" : "light"
 			authClass.setDefaultSiteTheme(newSiteTheme)
-			// if (!_.isNull(fortunaApiClient.httpClient.accessToken)) {
-			// 	const siteThemeResponse = await fortunaApiClient.personalInfoDataService.setDefaultSiteTheme(newSiteTheme)
-			// 	if (!_.isEqual(siteThemeResponse.status, 200) || isErrorResponse(siteThemeResponse.data)) {
-			// 		throw Error("Unable to save new default site theme")
-			// 	}
-			// }
 		} catch (error) {
 			console.error(error)
 		}
